d7ff5205b98366adad8909f332733745c20cee20
Gballado <95064164+Gballado@users.noreply.github.com>
2023-05-09 10:50:49 -0500

    Transformed the form
    Made the form use user input rather than hard coded dummy data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,50 @@
-import MeetupList from "../components/meetups/MeetupList";
+import { useState } from "react";
 
-// Render a list of data by mapping it. No matter the list of jsx data, now we can use it as an array and apply the same concept.
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import MeetupList from "../components/meetups/MeetupList";
+import { useEffect } from "react";
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-488e1-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        // Set to false when we have the data
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,18 +1,24 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     // Create a post request to firebase
     fetch(
       "https://react-getting-started-488e1-default-rtdb.firebaseio.com/meetups.json",
       {
-        method: 'POST', 
+        method: "POST",
         body: JSON.stringify(meetupData),
-        headers:{
-          'Conetent-Type': 'application/json'
-        }
+        headers: {
+          "Conetent-Type": "application/json",
+        },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
