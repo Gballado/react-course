fe758a71414901e920ec5feef184fba9e761fe4c
Gballado <95064164+Gballado@users.noreply.github.com>
2023-05-09 08:51:58 -0500

    Update NewMeetupForm.js
    Added functionality to the form, handling the submission and using ref

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,30 +1,66 @@
+// Using a reference to store values once the form is submitted
+import { useRef } from "react";
+
 import Card from "../ui/Card";
-import classes from './newMeetupForm.module.css';
+import classes from "./newMeetupForm.module.css";
 
 function NewMeetupForm() {
-  return <Card>
-    <form className={classes.form}>
+  // Using refs to read the value
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  // Creating a handler for the submit button
+  function submitHandler(event) {
+    // Prevents the browser default of creating a request and allows us to fully handle the submission
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetupData);
+  }
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
-            <label htmlFor='title'>Meetup Title</label>
-            <input type='text' required id='title'/>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
-            <label htmlFor='image'>Meetup Image</label>
-            <input type='url' required id='image'/>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
-            <label htmlFor='address'>Address</label>
-            <input type='text' required id='address'/>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={classes.control}>
-            <label htmlFor='description'>Description</label>
-            <textarea id="description" required rows='5'></textarea>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
         </div>
         <div className={classes.actions}>
-            <button>Add Meetup</button>
+          {/* If you have a button on a form, that button will be ommited */}
+          <button>Add Meetup</button>
         </div>
-    </form>
-  </Card>;
+      </form>
+    </Card>
+  );
 }
 
 export default NewMeetupForm;
